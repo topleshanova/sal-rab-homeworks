2b5e13f6e9bae370d035c9d6aa50882a6f86847e
Jenkins Fullstack <blednov.evg@yandex.ru>
2022-10-23 20:16:31 +0300

    Выполнение задания 3.1

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,7 +1,7 @@
 function renderCartItem(item) {
     const product = item;
 
-    // Задание №3.1. Формирование строки корзины
+    // Задание №3.1. Формирование строки корзины/
 
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
